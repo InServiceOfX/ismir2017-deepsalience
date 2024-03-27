b1562885d62d8df5aaf619f73a72e170cc1c2e08
Or-Tal <ortal1602@gmail.com>
2024-03-27 13:35:07 +0000

    commit changes

--- predict/saliency_cosine_sim.py
+++ predict/saliency_cosine_sim.py
@@ -6,29 +6,30 @@ from predict_saliency import init_predict_saliency_map_args, predict_saliency_ma
 import numpy as np
 import torch
 
+SRC_DIR = 'background_32k'
+TGT_DIR = 'tests_32k'
+
+
 def main(args):
     # create output directory if it does not exist
     os.makedirs(args.out_dir, exist_ok=True)
 
-    saliency_of_GT_dir = os.path.join(args.out_dir, 'GT')
-    saliency_of_generated_dir = os.path.join(args.out_dir, 'generated')
+    saliency_of_GT_dir = os.path.join(args.out_dir, SRC_DIR)
+    saliency_of_generated_dir = os.path.join(args.out_dir, TGT_DIR)
     os.makedirs(saliency_of_GT_dir, exist_ok=True)
     os.makedirs(saliency_of_generated_dir, exist_ok=True)
 
-    os.makedirs(args.out_dir, exist_ok=True)
-    os.makedirs(args.out_dir, exist_ok=True)
-
     # list all audio files in args.src_files using librosa
-    gt_files = librosa.util.find_files(f"{args.src_files}/GT", ext='wav')
-
+    gt_files = librosa.util.find_files(f"{args.src_files}/{SRC_DIR}", ext=args.file_format)
+    assert len(gt_files) > 0, f"No audio files in {args.src_files}/{SRC_DIR}"
     track_names = [os.path.basename(f).split('.')[0] for f in gt_files]
 
     EPS = 1e-8
     sum_cosine_similarity = 0
     total_timesteps = 0
     for trk in tqdm.tqdm(track_names):
-        gt_file = f"{args.src_files}/GT/{trk}.wav"
-        generated_file = f"{args.src_files}/generated/{trk}.wav"
+        gt_file = f"{args.src_files}/{SRC_DIR}/{trk}.{args.file_format}"
+        generated_file = f"{args.src_files}/{TGT_DIR}/{trk}.{args.file_format}"
         if not os.path.exists(gt_file):
             raise ValueError(f"File {gt_file} does not exist")
         if not os.path.exists(generated_file):
@@ -54,10 +55,11 @@ def main(args):
         sum_cosine_similarity += cosine_sim.sum().item()
         total_timesteps += cosine_sim.size(0)
         
-        # accumulate the cosine similarity scores
-        print(f"Track: {trk}, Cosine Similarity: {cosine_sim.mean().item()}")
+        # # accumulate the cosine similarity scores
+        # print(f"Track: {trk}, Cosine Similarity: {cosine_sim.mean().item()}")
     
     print(f"Average Cosine Similarity: {sum_cosine_similarity / total_timesteps}")
+    print("done")
     
 
 if __name__ == '__main__':
@@ -65,6 +67,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Evaluate cosine similarity between pairs of saliency maps")
     parser.add_argument("--src_files", type=str, help="Path to the list of audio files", required=True)
     parser.add_argument("--out_dir", type=str, help="Path to output directory", required=True)
+    parser.add_argument("--file_format", type=str, help="audio file format", default="wav")
     parser.add_argument("--saliency_threshold", type=float, default=0, help="Threshold for saliency map")
     args = parser.parse_args()
     main(args)  # Call the main function
